6de5387101137509cf11a5870f1cc134eaebd578
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-18 01:35:54 +0300

    logout view

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, logout_then_login, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('watch.urls')),
     path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/', views.LogoutView.as_view(next_page='login'), name='logout'),   
 ]
