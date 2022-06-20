63d7bf4ee7c968c7959dfd755911c80fc2845b42
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-20 04:45:53 +0300

    class Neighbourhood

--- watch/models.py
+++ watch/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from tinymce.models import HTMLField
 
 # Create your models here.
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    admin = models.ForeignKey(User,on_delete = models.CASCADE,related_name='administration',null=True)
+    image = CloudinaryField('image')
+    description = models.CharField(max_length=250)
+    occupants = models.IntegerField(default=0, null=True, blank=True)
+    posted_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name 
+    
+    def create_neighborhood(self):
+        """
+        A method that creates a neighbourhood
+        """
+        self.save()
+        
+    def delete_neighborhood(self):
+        """
+        A method that deletes a neighbourhood
+        """
+        self.delete()    
+        
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        """
+        A method that finds a neighbourhood using its id
+        """
+        return cls.objects.filter(id=neighborhood_id) 
+    
+    @classmethod
+    def update_neighbourhood(cls, id):
+        """
+        A method that updates a neighbourhood
+        """
+        neighbourhood = cls.objects.filter(id=id).update(id=id)
+        return neighbourhood    
