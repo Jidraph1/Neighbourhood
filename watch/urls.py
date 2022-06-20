3eac97804f44f83c2eb630b7fec96f3783ae3726
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-20 12:47:36 +0300

    update url

--- watch/urls.py
+++ watch/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 #create Urls
-urlpatterns =[
-    path('', views.index, name= 'index'),
-    
-]
+urlpatterns=[
+   path('',views.index, name='index'),
+   path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html')),
+   path('update-profile',views.update_profile, name='update_profile'), 
